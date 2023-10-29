cf0a5cf86804bda83b60dcf630d81801eb977e09
Xavier Sánchez Atienza <xaviersanchez@dorna.com>
2023-10-29 20:24:42 +0100

    feature: add some comments

--- src/WinSystems.Challenge.Core/Main.cs
+++ src/WinSystems.Challenge.Core/Main.cs
@@ -16,6 +16,11 @@ public class Main
     }
     #endregion
 
+    /// <summary>
+    /// Check if the encode string is correct
+    /// </summary>
+    /// <param name="challenge">the challenge object</param>
+    /// <exception cref="Exception">Encode string is wrong</exception>
     private async Task CheckFinalResult(ChallengeEntity challenge)
     {
         await Task.Delay(ChallengeEntity.ApiRequestLimits);
@@ -25,13 +30,24 @@ public class Main
         await Task.Delay(ChallengeEntity.ApiRequestLimits);
 
         if (!result)
-            throw new HttpRequestException("Order method was wrong");
+            throw new Exception("Encode string is wrong");
     }
 
+    /// <summary>
+    /// Check if two blocks are sequential.
+    ///
+    /// WinSystem API do the hard work.
+    /// </summary>
+    /// <param name="challenge">The challenge object</param>
+    /// <param name="firstBlock">First block of sequence</param>
+    /// <param name="secondBlock">Second block of sequence</param>
+    /// <returns>If the couple is sequential</returns>
     private async Task<bool> CheckBlocks(ChallengeEntity challenge, string firstBlock, string secondBlock)
     {
+        // Check the order through API
         var result = await _service.CheckOrder(new[] { firstBlock, secondBlock });
         
+        // Increment Block Check stats counter
         challenge.Stats.IncBlocksCall();
         
         await Task.Delay(ChallengeEntity.ApiRequestLimits);
@@ -39,6 +55,10 @@ public class Main
         return result;
     }
 
+    /// <summary>
+    /// Sets manually the last element of blocks
+    /// </summary>
+    /// <param name="challenge">All the challenge object</param>
     private void SetLastElement(ChallengeEntity challenge)
     {
         if (challenge.OrderedBlocks == challenge.Blocks2Order - 1)
@@ -49,23 +69,33 @@ public class Main
     {
         try
         {
+            // Start the challenge
             challenge.Start();
 
-            int blockPosition = challenge.OrderedBlocks;
+            // set the position to check the sequence
+            var blockPosition = challenge.OrderedBlocks;
+            
+            // order the blocks until all of them are ordered
             while (challenge.OrderedBlocks < challenge.Blocks2Order - 1)
             {
+                // life jacket for blocks array
                 if (blockPosition > challenge.Blocks2Order)
                     break;
 
+                // get 2 blocks to check
                 var refererBlock = challenge.Blocks[challenge.OrderedBlocks - 1];
                 var block2Check = challenge.Blocks[blockPosition];
 
+                // check the blocks
                 var result = await CheckBlocks(challenge,refererBlock, block2Check);
 
+                // if are sequential then...
                 if (result)
                 {
+                    // set the new block to the encode array
                     challenge.SetOrderedBlock(block2Check);
 
+                    // if the two blocks not are sequential then reorder the blocks array
                     if (blockPosition != challenge.OrderedBlocks)
                     {
                         var wildBlock = challenge.Blocks[challenge.OrderedBlocks];
@@ -74,24 +104,33 @@ public class Main
                         challenge.Blocks[blockPosition] = wildBlock;
                     }
 
+                    // reindex the unordered position and set the new position to check
                     blockPosition = challenge.OrderedBlocks++;
                 }
                 else
                     blockPosition++;
             }
 
+            // Set the last element manually
             SetLastElement(challenge);
 
+            // Check the encode order
             await CheckFinalResult(challenge);
         }
         finally
         {
+            // Stop the challenge
             challenge.Stop();
         }
 
         return challenge.Blocks;
     }
 
+    /// <summary>
+    /// Method in charge of obtaining the token from the API
+    /// </summary>
+    /// <param name="email">Authentication user</param>
+    /// <returns>JWT auth</returns>
     private async Task<string> SignIn(string email)
     { 
         var token = await _service.GetToken(email);
